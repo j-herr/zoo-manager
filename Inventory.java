cb6b13bfb9468063cfa93f1f7b0d11cb51af5034
Rmmvx <57759727+Rmmvx@users.noreply.github.com>
2019-12-05 00:40:57 -0500

    Delete Inventory.java

--- Inventory.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-public class Inventory
-{
-   //Will keep track of gifts added to Inventory
-   public static ArrayList<Gift> inStock = new ArrayList<Gift>();
-   
-   //will keep track of gifts added to basket
-   public static ArrayList<Gift> inBasket = new ArrayList<Gift>();
-}
