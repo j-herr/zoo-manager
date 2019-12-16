edc8adafa3971843e94b0e0b7378ac8506c7fe80
Benjamin Buchanan <57759755+Benjino1398@users.noreply.github.com>
2019-12-15 22:38:39 -0500

    Add files via upload

---
+++ ChoosingRestaurant.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class ChoosingRestaurant {
+   public static void main(String[] args) {
+      JFrame frame = new JFrame("Choose Which Restaurant");
+      frame.setLocationRelativeTo(null);
+      frame.setSize(100,150);
+      JPanel panel = new JPanel();
+      panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+      JButton rest1 = new JButton("Hot Dog Pound");
+      JButton rest2 = new JButton("Senor Sapo's");
+      JButton done = new JButton("Done");
+      
+      panel.add(rest1);
+      panel.add(rest2);
+      panel.add(done);
+      frame.add(panel);
+      frame.setVisible(true);
+      
+      rest1.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            EateryOne.main(null);
+            frame.dispose();
+            }});
+      rest2.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            EateryTwo.main(null);
+            frame.dispose();
+            }});
+      done.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            ZooUI.main(null);
+            frame.dispose();
+            }}); 
+  
+   }
+   
+   
+   
+   }
---
+++ EateryOne.java
@@ -0,0 +1,201 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class EateryOne { 
+   
+   public static ArrayList<EateryOne> menu = new ArrayList<EateryOne>();
+   public static String restName = "The Hot Dog Pound";
+   private int unitPrice; private int unitsSold; private String unitName;
+   
+   public EateryOne(String unitName) { this.setUnitName(unitName); menu.add(this); }
+   public EateryOne(int unitPrice, int unitsSold, String unitName) {
+      this.setUnitPrice(unitPrice);
+      this.setUnitsSold(unitsSold);
+      this.setUnitName(unitName);
+      menu.add(this); 
+      }
+      
+   public int calcRevenue() { return this.unitPrice * this.unitsSold; }
+   
+   public void setUnitName(String unitName) { this.unitName = unitName; }
+   public void setUnitPrice(int unitPrice) { this.unitPrice = unitPrice; }
+   public void setUnitsSold(int unitsSold) { this.unitsSold = unitsSold; }  
+   
+   public int getUnitPrice() { return this.unitPrice; }
+   public int getUnitsSold() { return this.unitsSold; }
+   public String getUnitName() { return this.unitName; }
+  
+
+   public void editItem() {
+      int tempPrice;
+      int tempAmount;
+      
+      JFrame frame = new JFrame();
+      frame.setLocationRelativeTo(null);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(200,200);
+      JPanel panel = new JPanel();
+      panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+      
+      
+      JLabel header = new JLabel(this.getUnitName());
+      header.setFont(new Font("Serif", Font.BOLD, 20));   
+      JLabel enterAmountLabel = new JLabel("Enter amount of item sold: ");
+      JLabel enterPriceLabel = new JLabel("Enter price of item: ");     
+      JTextField enterAmountField = new JTextField(10);
+      JTextField enterPriceField = new JTextField(10);
+      JButton confirm = new JButton("Confirm");
+      JButton reset = new JButton("Reset");
+   
+      panel.add(header);
+      panel.add(enterAmountLabel);
+      panel.add(enterAmountField);
+      panel.add(enterPriceLabel);
+      panel.add(enterPriceField);
+      panel.add(confirm);
+      panel.add(reset);  
+      
+      confirm.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            setUnitPrice(Integer.parseInt(enterPriceField.getText()));
+            setUnitsSold(Integer.parseInt(enterAmountField.getText()));
+            main(null);
+            frame.dispose();
+         }});
+      reset.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+         enterAmountField.setText("0");
+         enterPriceField.setText("0");
+         }});
+      
+      
+      
+      
+      frame.add(panel);
+      frame.setVisible(true);
+   }
+
+  
+  public static void afterFinish() {
+      JFrame frame = new JFrame();
+      //frame.setLocationRelativeTo(null);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(250,500);
+      frame.setLayout(new BorderLayout());
+      JPanel panel = new JPanel();
+      panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+      JTextArea output = new JTextArea();
+      JButton confirm = new JButton("Confirm");
+      JPanel panel2 = new JPanel();
+      
+      output.append(" -Revenue for each menu item-");
+      for(EateryOne x : menu) {
+         output.append(x.toString());
+         System.out.print("\n"); 
+        }
+      
+      confirm.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            ChoosingRestaurant.main(null);
+            frame.dispose();
+            }}); 
+
+   
+   
+      panel.add(output);
+      panel2.add(confirm);
+      frame.add(panel);
+      frame.add(panel2, BorderLayout.SOUTH);
+      frame.setVisible(true);
+   
+ } 
+  
+
+   @Override
+   public String toString() { return "\n" + "\n Item: " + this.getUnitName() + "\n Units sold: " + this.getUnitsSold() + 
+                                     "\n Item Price: " + this.getUnitPrice() + "\n Revenue for item: " + this.calcRevenue();
+                            }
+
+public static void main(String[] args) {
+   EateryOne entreeOne = new EateryOne("Classic Dog");
+   EateryOne entreeTwo = new EateryOne("Chicago Dog");
+   EateryOne entreeThree = new EateryOne("The Big Dog");
+   EateryOne sideOne = new EateryOne("Fries");
+   EateryOne sideTwo = new EateryOne("Dog Collars");
+   
+   JFrame frame = new JFrame();
+   frame.setLocationRelativeTo(null);
+   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   frame.setSize(150,250);
+   JPanel panel = new JPanel();
+   panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+      
+   JLabel choice = new JLabel("Select an item to edit"); 
+   JButton entOneButton = new JButton(entreeOne.getUnitName());   
+   JButton entTwoButton = new JButton(entreeTwo.getUnitName());
+   JButton entThreeButton = new JButton(entreeThree.getUnitName());
+   JButton sideOneButton = new JButton(sideOne.getUnitName());
+   JButton sideTwoButton = new JButton(sideTwo.getUnitName());
+   JButton finish = new JButton("Finish");
+   
+   panel.add(choice);
+   panel.add(entOneButton);
+   panel.add(entTwoButton);
+   panel.add(entThreeButton);
+   panel.add(sideOneButton);
+   panel.add(sideTwoButton);
+   panel.add(finish);
+   
+   entOneButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+      entreeOne.editItem();
+      frame.dispose();
+      }});
+
+   
+   entTwoButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+      entreeTwo.editItem();
+      frame.dispose();
+      }});
+
+   
+   entThreeButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+      entreeThree.editItem();
+      frame.dispose();
+      }});
+
+   
+   sideOneButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+      sideOne.editItem();
+      frame.dispose();
+      }});
+
+   
+   sideTwoButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+      sideTwo.editItem();
+      frame.dispose();
+      }});
+   
+   finish.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+      afterFinish();
+      frame.dispose();
+      }});
+   
+   
+   
+   
+   
+   
+   frame.add(panel);
+   frame.setVisible(true);
+   
+   
+   }
+}
